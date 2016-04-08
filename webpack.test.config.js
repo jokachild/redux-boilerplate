dfc4d933f25cf5e975c6d02c67649aae80e73ca4
Andrey Myroniuk <andrej.mironjuk@gmail.com>
2016-04-08 23:36:45 +0300

    Fixed package.json

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -10,7 +10,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js|\.jsx$/,
-                loader: "babel-loader",
+                loader: "babel",
                 exclude: /node_modules/
             }
         ]
